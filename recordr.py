e49d14e0867d60b58b097166e533b77aaf5cfaac
Alicja Krupa <mintyasleep@protonmail.com>
2024-09-04 01:31:28 +0100

    untested manual mode

--- recordr.py
+++ recordr.py
@@ -31,189 +31,15 @@ except:
 
 
 
-def clver():
-    # SECTION A
-    with open("discogstoken2",'r') as token_file:
-        token = token_file.read().strip()
-
-    d = discogs_client.Client('mintyrecordmetadata/0.1', user_token=token)
-
-
-    query = input("Search for name of Album or Release >")
-
-    results = d.search(query,type='release',format='Vinyl')
-    i = 0
-    found = False
-    collection_text = ''
-    for result in results:
-        #print(result)
-        if found == False:
-            if len(d.identity().collection_items(result.id)) > 0:
-                found = True
-                collection_text = ' <<IN COLLECTION>>'
-            else:
-                collection_text = ' <<NOT FOUND>>'
-        print(str(i) + ' ==> ' + result.labels[0].catno + ': ' + result.artists[0].name + ' - ' + result.title + ' (' + result.country + ') - ' + result.formats[0]['name'] + ' - Discogs url: https://www.discogs.com/release/' + str(result.id) + collection_text)
-        #print(result.country)
-        #print(result.formats[0]['name'])
-        i += 1
-        collection_text = ' <<UNCHECKED>>'
-
-    option = -1
-    while option < 0 or option >= len(results):
-        try:
-            option = int(input('Select number >'))
-        except:
-            print('Select valid number')
-
-    album_id = results[option].id
-
-
-        
-
-
-    # SECTION B
-    album = d.release(album_id)
-    tracklist = album.tracklist
-
-    if tracklist[0].position != 'A1':
-        print('something is wrong?')
-
-    side = ''
-    while side != 'A' and side != 'B':
-        side = input('Select side (A or B) >').upper()
-
-
-    side_tracklist = []
-
-    first_track_no = -1
-    c_track_no = 1
-    for track in tracklist:
-        if track.position[0] == side:
-            side_tracklist.append(track)
-            if first_track_no == -1:
-                first_track_no = c_track_no
-        c_track_no += 1
-
-    for track in side_tracklist:
-        print(track)
-
-    mus_position = 0.0
-    song_starts = []
-
-    argv = sys.argv
-    filename = str(argv[1])
-
-    recordfile = AudioSegment.from_file(filename)
-
-    print('Finding breaks in file (may take some time)')
-    silence = silence.detect_silence(recordfile, min_silence_len=1000, silence_thresh=ST, seek_step=10)
-
-    silence = [((start/1000),(stop/1000)) for start,stop in silence] #convert to sec
-
-
-
-    # print(silence)
-
-
-
-    for i in range(0,len(side_tracklist)-1):
-        print('Song ' + side_tracklist[i].position + ' (' + side_tracklist[i].title + ') starts at ' + str(mus_position))
-        next_silence = [(stop,stop-start) for start,stop in silence if stop > mus_position]
-        print('Select start position for song ' + side_tracklist[i+1].position + ' (' + side_tracklist[i+1].title + ')')
-        for j,s in enumerate(next_silence):
-            print(str(j) + ': ' + str(s[0]) + 's (' + str(int(s[0])//60) + 'm ' + str(int(s[0]) % 60) +'s) - Gap duration: ' + str(round(s[1],2)) + 's')
-        option = -1
-        while option < 0 or option >= len(next_silence):
-            try:
-                option = int(input('>'))
-            except:
-                print('Select valid number')
-        mus_position = next_silence[option][0]
-        song_starts.append(mus_position)
-
-    song_starts = [song - TBS for song in song_starts]
-
-    song_starts.insert(0,0.0)
-
-    print(song_starts)
-    for i in range(0,len(song_starts) - 1):
-        song_audio = recordfile[song_starts[i]*1000:song_starts[i+1]*1000]
-        song_audio.export(str(i) + FE,format=FF)
-
-
-    song_audio = recordfile[song_starts[len(song_starts) - 1]*1000:]
-    song_audio.export(str(len(song_starts) - 1) + FE,format=FF)
-
-    o_album_artist = ''
-    for artist in album.artists:
-        o_album_artist += artist.name
-        if artist.join != '':
-            o_album_artist += ' ' + artist.join + ' '
-
-    '''
-    o_album = album.title
-    o_artist = o_album_artist
-    o_artwork
-    o_total_tracks = len(album.tracklist)
-    o_year = album.released
-    o_comment = 'Vinyl Rip'
-    o_genre = album.genres[0]
-    '''
-    url = album.images[0]['uri']
-    #urllib.request.urlretrieve(url,"albumart.jpg")
-    #r = requests.get(url)
-    #with open('albumart.jpg', 'wb') as albumfile:
-    #    albumfile.write(r.content)
-
-    try:
-        opener = urllib.request.URLopener()
-        opener.addheader('User-Agent', 'minty metadata automate')
-        filename, headers = opener.retrieve(url, 'albumart.jpg')
-    except:
-        print("Failed to retrieve album art. Please download the following image and save it in the same folder as albumart.jpg")
-        print(url)
-        whocares = input("Press enter when done")
-
-        
-
-    for i in range(0,len(side_tracklist)):
-        print('Tagging file ('+str(i+1) + '/' + str(len(side_tracklist)) + ')... ',end='')
-        s = music_tag.load_file(str(i) + FE)
-        track = side_tracklist[i]
-        s['tracktitle'] = track.title
-        s['album'] = album.title
-        s['artist'] = o_album_artist
-        s['albumartist'] = o_album_artist
-        s['year'] = album.year
-        s['genre'] = album.genres[0]
-        s['tracknumber'] = first_track_no + i
-        s['totaltracks'] = len(album.tracklist)
-        with open('albumart.jpg','rb') as img_in:
-            s['artwork'] = img_in.read()
-        #s['artwork'].first.thumbnail([64,64])
-        #s['artwork'].first.raw_thumbnail([64,64])
-        s.save()
-
-
-        # check varibale bit rate if ths doesn't fix it
-        
-        ffmpeg = (
-            FFmpeg()
-            .input(str(i) + FE)
-            .output(str(first_track_no + i).zfill(2) + ' ' + track.title + ' - ' + o_album_artist + FE)
-        )
-        ffmpeg.execute()
-        os.remove(str(i) + FE)
-        print('Done.')
-        
-        
-        #os.replace("reencode" + str(i) + FE,str(first_track_no + i).zfill(2) + ' ' + track.title + ' - ' + o_album_artist + FE)
 
 def searchio():
-    search_button.state(['disabled'])
+    disableui()
     feedback.set("Searching, please wait warmly...")
-    recordr.after(10,search_start)
+    if len(search.get()) > 0:
+        recordr.after(10,search_start)
+    else:
+        enableui()
+        feedback.set("Please enter a search query")
 
 
 def search_start(*args):
@@ -248,7 +74,17 @@ def search_continue():
             else:
                 collection_text = ' <<NOT FOUND>>'
         feedback.set("Progress: " + str(round( ((i+1) / len(results)) * 100 )) + "%")
-        resulttext.append(str(i) + ' ==> ' + result.labels[0].catno + ': ' + result.artists[0].name + ' - ' + result.title + ' (' + result.country + ') - ' + result.formats[0]['name'] + collection_text)
+        try:
+            resulttext.append(str(i) + ' ==> ' + result.labels[0].catno + ': ' + result.artists[0].name + ' - ' + result.title + ' (' + result.country + ') - ' + result.formats[0]['name'] + collection_text)
+        except:
+            try:
+                resulttext.append(str(i) + ' ==> ' + result.labels[0].catno + ': ' + result.artists[0].name + ' - ' + result.title + ' - ' + result.formats[0]['name'] + collection_text)
+            except:
+                try:
+                    resulttext.append(str(i) + ' ==> ?: ' + result.artists[0].name + ' - ' + result.title + ' - ' + collection_text)
+                except:
+                    resulttext.append("Release doesn't exist")
+
         resulthl.append(result.id)
         #print(result.country)
         #print(result.formats[0]['name'])
@@ -258,7 +94,7 @@ def search_continue():
     else:
         feedback.set("Search done")
         sresults['values'] = resulttext
-        search_button.state(['!disabled'])
+        enableui()
 
 def hyperlink_update(*args):
     #global album,d,tracklist
@@ -276,6 +112,7 @@ def file_browse():
     filenamevar.set(filename)
 
 def find_breaks():
+    disableui()
     try:
         float(tbs_var.get())
         float(st_var.get())
@@ -304,6 +141,11 @@ def breaks_process():
     for j,s in enumerate(next_silence):
         readable_sl.append(str(j) + ': ' + str(s[0]) + 's (' + str(int(s[0])//60) + 'm ' + str(int(s[0]) % 60) +'s) - Gap duration: ' + str(round(s[1],2)) + 's')
     breaksvar.set(readable_sl)
+    enableui()
+
+def side_updateio():
+    disableui()
+    recordr.after(10,side_update)
 
 def side_update():
     global album,d,tracklist,side_tracklist,first_track_no
@@ -370,36 +212,61 @@ def side_update():
         song_numvar.set("Songs found: " + str(len(side_tracklist)))
         for track in side_tracklist:
             print(track)
+    enableui()
 
 
 def songnumber_update(*args):
     global side_tracklist
     print(breaks_listbox.curselection())
-    try:
-        song_numvar.set("Selected: " + str(len(breaks_listbox.curselection())) + "/" + str(len(side_tracklist)))
-        if len(breaks_listbox.curselection()) == len(side_tracklist):
-            gobutton.state(['!disabled'])
-        else:
+    if manual_entry.get() == True:
+        song_numvar.set("Using manual entry")
+        gobutton.state(['!disabled'])
+    else:
+        try:
+            song_numvar.set("Selected: " + str(len(breaks_listbox.curselection())) + "/" + str(len(side_tracklist)))
+            if len(breaks_listbox.curselection()) == len(side_tracklist):
+                gobutton.state(['!disabled'])
+            else:
+                gobutton.state(['disabled'])
+        except:
+            song_numvar.set("Selected: " + str(len(breaks_listbox.curselection())))
             gobutton.state(['disabled'])
-    except:
-        song_numvar.set("Selected: " + str(len(breaks_listbox.curselection())))
-        gobutton.state(['disabled'])
 
 def metadata_io(*args):
-    recordr.after(10,metadata_process)
+    global side_tracklist,song_starts
+    
+    disableui()
+    gobutton.state(['disabled'])
+
+    continute_mtdt = False
+    if manual_entry.get() == True:
+        song_starts = []
+        try:
+            for value_i in manual_text.get().split(","):
+                song_starts.append(float(value_i))
+            if len(side_tracklist) == len(song_starts):
+                continute_mtdt = True
+        except:
+            pass
+    if continute_mtdt == True:
+        recordr.after(10,metadata_process)
+    else:
+        enableui()
 
 def metadata_process():
-    global side_tracklist,next_silence,silencelist,recordfile
+    global side_tracklist,next_silence,silencelist,recordfile,song_starts
 
 
     TBS = float(tbs_var.get())
     
-    song_starts = []
 
-    for sel in breaks_listbox.curselection():
-        #mus_position = next_silence[sel][0]
-        #song_starts.append(mus_position)
-        song_starts.append(next_silence[sel][0])
+
+    if manual_entry.get() != True:
+        song_starts = []
+        for sel in breaks_listbox.curselection():
+            #mus_position = next_silence[sel][0]
+            #song_starts.append(mus_position)
+            song_starts.append(next_silence[sel][0])
 
     
     
@@ -471,7 +338,21 @@ def metadata_process():
         ffmpeg.execute()
         os.remove(str(i) + FE)
         print('Done.')
+    enableui()
+    gobutton.state(['!disabled'])
 
+def disableui():
+    search_button.state(['disabled'])
+    update_button.state(['disabled'])
+    browse_button.state(['disabled'])
+    breaks_button.state(['disabled'])
+    #gobutton.state(['disabled'])
+
+def enableui():
+    search_button.state(['!disabled'])
+    update_button.state(['!disabled'])
+    browse_button.state(['!disabled'])
+    breaks_button.state(['!disabled'])
 
 try:
     with open("discogstoken2",'r') as token_file:
@@ -537,7 +418,8 @@ side_box['values'] = ['A','B','C','D','One Side','Single (Side A)','Single (Side
 #sresults.bind('<<ComboboxSelected>>',side_update)
 
 
-ttk.Button(uframe,text="Update Song Information",command=side_update).grid(row=8,column=1,columnspan=2,sticky=(W,E))
+update_button = ttk.Button(uframe,text="Update Song Information",command=side_updateio)
+update_button.grid(row=8,column=1,columnspan=2,sticky=(W,E))
 
 
 #divider
@@ -549,7 +431,8 @@ filenamevar = StringVar()
 filename_entry = ttk.Entry(uframe,width=40,textvariable=filenamevar)
 filename_entry.grid(column=1,row=12,columnspan=2,sticky=(W,E))
 
-ttk.Button(uframe,text="Browse",command=file_browse).grid(row=11,column=2,columnspan=1,sticky=(W,E))
+browse_button = ttk.Button(uframe,text="Browse",command=file_browse)
+browse_button.grid(row=11,column=2,columnspan=1,sticky=(W,E))
 
 #gaps
 ttk.Label(uframe,text="Time before song").grid(column=1,row=13,sticky=(W,E))
@@ -563,23 +446,35 @@ st_var.set(str(ST))
 ttk.Spinbox(uframe,from_=-100, to=0,textvariable=st_var,increment=1).grid(column=2,row=14,sticky=(W,E))
 
 
-ttk.Button(uframe,text="Find breaks in file",command=find_breaks).grid(row=15,column=1,columnspan=2,sticky=(W,E))
+breaks_button = ttk.Button(uframe,text="Find breaks in file",command=find_breaks)
+breaks_button.grid(row=15,column=1,columnspan=2,sticky=(W,E))
 
 #breaks results
 song_numvar = StringVar()
 song_numvar.set("Update song information to see track data")
-ttk.Label(uframe,textvariable=song_numvar).grid(row=2,column=5,sticky=(W,E))
+ttk.Label(uframe,textvariable=song_numvar).grid(row=2,column=5,columnspan=2,sticky=(W,E))
 
 breaksvar = StringVar()
 breaks_listbox = Listbox(uframe,height=14,width=45,listvariable=breaksvar,selectmode=EXTENDED)
-breaks_listbox.grid(column=5,row=3,rowspan=18,sticky=(W,E,N,S))
+breaks_listbox.grid(column=5,row=3,rowspan=12,columnspan=2,sticky=(W,E,N,S))
 
 breaks_listbox.bind("<<ListboxSelect>>",songnumber_update)
+
+#manual entry
+
+manual_entry = BooleanVar(value=False)
+manual_entry_check = ttk.Checkbutton(uframe,text="Manual Entry",variable=manual_entry,command=songnumber_update)
+manual_entry_check.grid(column=5,row=15,sticky=(W,E))
+
+manual_text = StringVar()
+ttk.Entry(uframe,width=30,textvariable=manual_text).grid(column=6,row=15,sticky=(W,E))
+
+
 
 
 #GO!!!!!!!!!!!!!!!
 gobutton = ttk.Button(uframe,text="Process",command=metadata_io)
-gobutton.grid(column=5,row=21,sticky=(W,E))
+gobutton.grid(column=5,row=21,columnspan=2,sticky=(W,E))
 gobutton.state(['disabled'])
 
 
